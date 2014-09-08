805581187b236fec99cd4c00d004f752fa10b781
Jae Choi <jaekun.choi@gmail.com>
2014-09-09 01:17:25 +1000

    Finished maximizing xor challenge

--- maximizing_xor.cpp
+++ maximizing_xor.cpp
@@ -1,25 +1,32 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 #include "maximizing_xor.h"
 
 using namespace std;
 
-int maxXor(int _l, int _r)
-{
-    cout << _l << _r << endl;
-    return 0;
+int maxXor(int _l, int _r) {
+    int results = _l ^_r;
+    return results;
 }
 
-maximizing_xor::maximizing_xor()
-{
-    unsigned int L, R, A, B, res;
+maximizing_xor::maximizing_xor() {
+    int L, R, A, B, res;
+    vector<int> xor_results;
+
     cin >> A >> B;
-    res = maxXor(A, B);
 
-    cout << res << endl;
+    for (; A <= B; ++A) {
+        for (int C=A; C <= B; ++C) {
+            res = maxXor(A, C);
+            xor_results.push_back(res);
+        }
+    }
+
+    int maximum_val = *max_element(xor_results.begin(), xor_results.end());
+    cout << maximum_val << endl;
 }
 
-maximizing_xor::~maximizing_xor()
-{
+maximizing_xor::~maximizing_xor() {
     //dtor
 }
